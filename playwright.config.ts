daff17a0b6372110bff8a9e18d770991def5a55b
YaninaQ1 <yq@qubikcommerce.com>
2026-02-19 15:58:14 -0300

    refactor: Rename `amazonPage` to `homePage`, migrate test logic to `challenge.spec.ts`, and add `slowMo` to Playwright config.

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,9 @@ export default defineConfig({
   ],
   use: {
     baseURL: process.env.BASE_URL,
+    launchOptions: {
+      slowMo: 500,
+    },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
---
+++ tests/specs/challenge.spec.ts
@@ -0,0 +1,24 @@
+
+import { test } from "../base";
+
+test('End to End test', async ({ homePage, page, loginPage }) => {
+    test.setTimeout(100000);
+
+    await loginPage.goToAmazonSite();
+    await homePage.closeLocationModal();
+
+    await homePage.searchProduct(process.env.PRODUCT_SEARCH!);
+    await homePage.validateSearchResults(process.env.PRODUCT_SEARCH!);
+
+    await homePage.filterBrand(process.env.BRAND_FILTER!);
+    await page.waitForTimeout(1000);
+
+    await homePage.sortBy(process.env.HIGT_PRICE_OPTION!);
+    await homePage.printTopProducts(5, true, process.env.COMMENT_HIGT_PRICE!);
+
+    await homePage.sortBy(process.env.NEW_RELEASES_OPTION!);
+    await homePage.printTopProducts(5, false, process.env.COMMENT_NEW_RELEASES!);
+
+    await homePage.sortBy(process.env.REVIEW_RANK_OPTION!);
+    await homePage.printTopProducts(5, true, process.env.COMMENT_REVIEW_RANK!);
+});
